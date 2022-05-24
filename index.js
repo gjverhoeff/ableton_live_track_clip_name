e812a6cdc4343af5a19be42ee0d89cb5f59d906a
Geert Verhoeff <g.verhoeff@eslgaming.com>
2022-05-24 16:17:55 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import { Ableton } from "ableton-js";
+import express from 'express';
+import * as fs from 'fs';
+
+
+
+const ableton = new Ableton();
+const app = express()
+const port = 3000
+var playing_song = ""
+
+
+app.get('/json', (req, res) => {
+        
+    res.send({
+        "playing_song": playing_song
+    })    
+
+})
+
+app.get('/name', (req, res) => {
+        
+    res.send(playing_song)    
+
+})
+
+
+app.listen(port, () => {
+  console.log(`Trackname is available on ${port}/name and as json on ${port}/json`)
+})
+
+
+
+
+const getPlayingClipTrack = async () => {
+  
+const tracks = await ableton.song.get("tracks");
+
+
+  let clip_slots = await tracks[2].get("clip_slots")
+  
+  clip_slots.forEach(async (slot) => {
+    if(slot.raw.is_playing === true){
+        let clip = await slot.get("clip")
+        const name = await clip.get("name");
+        playing_song = name
+        fs.writeFile('song.txt', name, function (err) {
+            if (err) return console.log(err);
+          });
+    }
+
+  });
+  };
+
+function run(){
+    setInterval(getPlayingClipTrack, 5000)
+}
+run();
